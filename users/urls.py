96e58676067b666519a279cd3af9db6adc9686bf
Siyue Zhou <sz132@duke.edu>
2018-02-12 21:51:57 -0500

    initial commit

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,5 @@ app_name = 'users'
 urlpatterns = [
     path('register/', views.register, name = 'register'),
     path('password/', views.change_password, name = 'change_password'),
-    path('password/', views.reset_password, name = 'reset_password'),
+    path('password/reset', views.reset_password, name = 'reset_password'),
 ]
